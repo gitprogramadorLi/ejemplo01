19816a2d9911b30f360be8dddc6ef66d23fc6520
gitprogramadorLi <90430863+gitprogramadorLi@users.noreply.github.com>
2021-09-10 00:20:07 -0400

    Add files via upload

---
+++ ejemplo01/Principal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Datos Personales \n " << endl;
+	cout << "Nombre: Lizeth Maribel Condori Cuiza \n " << endl;
+	cout << "Carrera: Ingenieria de Sistemas \n " << endl;
+	cout << "CI: 6692141 \n " << endl;
+	cout << "CU: 35-4895 \n " << endl;
+	return 0;
+
+}
